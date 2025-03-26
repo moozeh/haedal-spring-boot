c5b74fc72c7559906a87fc4ac46b2cea66d2eff9
moozeh <kcw2205@naver.com>
2025-03-26 19:10:20 +0900

    chore: Environment Settings
    - Spring initializr 로 초기화

---
+++ src/main/java/org/moozeh/haedalspringboot/HaedalSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.moozeh.haedalspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaedalSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaedalSpringBootApplication.class, args);
+	}
+
+}
